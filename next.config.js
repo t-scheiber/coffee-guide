4845555c6333099244a0460c46a7129f95b7133e
Thomas Scheiber <administrator@europetalks.eu>
2026-02-15 03:34:00 +0100

    chore: set turbopack.root to fix lockfile warning and clean build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Static export removed - using server-side rendering
+  turbopack: {
+    root: __dirname,
+  },
 }
 
 module.exports = nextConfig
